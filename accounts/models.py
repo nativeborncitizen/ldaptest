6ba909580b5898f1a73a114622d3f50fbbf01673
dsz <nativeborncitizen@gmail.com>
2013-11-12 16:40:17 +0200

    UserProfile save

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,18 @@
 #coding: utf-8
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
 
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     ip = models.CharField(max_length=30, blank=True)
+
+
+def create_profile(sender, **kw):
+    user = kw["instance"]
+    if kw["created"]:
+        user_profile = UserProfile(user=user)
+        user_profile.save()
+
+post_save.connect(create_profile, sender=User)
