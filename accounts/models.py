ab170d0a9a156dbbbeccaccbbfba6fa3077c581e
dsz <nativeborncitizen@gmail.com>
2013-11-12 15:02:45 +0200

    rm .pyc

--- accounts/models.py
+++
@@ -1,8 +0,0 @@
-#coding: utf-8
-from django.contrib.auth.models import User
-from django.db import models
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User)
-    ip = models.CharField(max_length=30, blank=True)
